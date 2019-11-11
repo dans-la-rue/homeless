a3db86de20401f917f0e0293fdc340ac9e36600a
blablanumerodeux <blablanumerodeux@gmail.com>
2019-11-10 22:35:37 -0500

    develop: fix issue with the deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x # Print commands and their arguments as they are executed.
 
-cd /opt/
+cd /home/dreaser/homeless/
 docker-compose -f docker-compose-prod.yml pull
 docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up -d
