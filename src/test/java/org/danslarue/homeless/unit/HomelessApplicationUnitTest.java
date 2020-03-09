aa08d39d20d68b2cdff57d8d9f8feca76c333530
blablanumerodeux <blablanumerodeux@gmail.com>
2020-03-09 11:17:32 -0400

    develop: unit tests fix

---
+++ src/test/java/org/danslarue/homeless/unit/HomelessApplicationUnitTest.java
@@ -0,0 +1,42 @@
+package org.danslarue.homeless.unit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.danslarue.homeless.models.Shelter;
+import org.danslarue.homeless.repositories.IShelterRepository;
+import org.danslarue.homeless.services.interfaces.IShelterService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+//@TestConfiguration
+public class HomelessApplicationUnitTest {
+
+    @Autowired
+    IShelterService shelterService;
+
+    @MockBean
+    IShelterRepository shelterRepository;
+
+    @Test
+    // check the business rules instead
+    public void gotAllShelter() {
+        List<Shelter> shelterList = new ArrayList<>();
+        shelterList.add(new Shelter());
+        when(shelterRepository.findAll()).thenReturn(shelterList);
+
+        shelterList = shelterService.allShelter();
+        assertNotNull(shelterList);
+    }
+}
